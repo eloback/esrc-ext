1e92306122b623cd7435cdf3931e1b8b6084e7fb
Mateus Barbosa <barbosamat16@gmail.com>
2025-10-08 09:03:29 -0300

    devenv: add PostgreSQL service configuration and environment variables for examples

--- devenv.nix
+++ devenv.nix
@@ -21,6 +21,35 @@ in
         };
       };
     }
+    (
+      _:
+      let
+        db_user = "nixus";
+        db_pass = "postgres";
+      in
+      {
+        custom.pgdatabase.enable = false;
+        services = {
+          postgres = {
+            enable = lib.mkDefault true;
+            initialDatabases = lib.mkDefault [
+              { name = "dead_letter_db"; }
+            ];
+            initdbArgs = [
+              "--locale=C"
+              "--encoding=UTF8"
+              "--username=${db_user}"
+            ];
+          };
+        };
+        env = {
+          DATABASE_URL = lib.mkDefault "postgres://${config.env.PGUSER}:${config.env.PGPASSWORD}@${lib.escapeURL config.env.DEVENV_RUNTIME}%2fpostgres/dead_letter_db";
+          PGUSER = lib.mkDefault db_user;
+          PGPASSWORD = lib.mkDefault db_pass;
+          PGDATABASE = lib.mkDefault cfg.common.project_name;
+        };
+      }
+    )
   ];
   config = {
     custom = {
