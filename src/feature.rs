c911086c3f6b6409198a8c717488940f52a2f061
Mateus Barbosa <barbosamat16@gmail.com>
2025-10-09 11:46:25 -0300

    feat: add dead letter automation and legacy project automation methods to Feature struct

--- src/feature.rs
+++ src/feature.rs
@@ -54,4 +54,48 @@ impl<'a> Feature<'a> {
                 .expect("automation should be able to start");
         });
     }
+    pub fn start_dead_letter_automation<A>(
+        &self,
+        durable_name: &'static str,
+        stream_name: &'static str,
+        feature_name: &'static str,
+        dead_letter_store: A,
+    ) where
+        A: esrc::nats::DeadLetterStore + Clone + 'static,
+    {
+        let store = self.store.clone();
+        let dead_letter_store = dead_letter_store.clone();
+
+        store.get_task_tracker().spawn(async move {
+            // Start dead letter automation for a specific stream and consumer
+            store
+                .run_dead_letter_automation(
+                    dead_letter_store,
+                    durable_name,
+                    stream_name,
+                    feature_name,
+                )
+                .await
+                .expect("dead letter automation should be able to start");
+        });
+    }
+}
+
+impl<'a> Feature<'a> {
+    pub fn start_legacy_automation<A>(
+        &self,
+        project: A,
+        feature_name: &'static str,
+        subjects: Vec<&'static str>,
+    ) where
+        A: esrc::nats::legacy::LegacyProject + 'static,
+    {
+        let store = self.store.clone();
+        store.get_task_tracker().spawn(async move {
+            store
+                .run_legacy_project(project, feature_name, subjects)
+                .await
+                .expect("automation should be able to start");
+        });
+    }
 }
