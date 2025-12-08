6bd1e07dc98dc5fbe14cb1bb9702704f79c6b7b3
Mateus Barbosa <barbosamat16@gmail.com>
2025-12-08 10:25:35 -0300

    docs: implement user translation project with NATS event handling example

---
+++ examples/translation_project/main.rs
@@ -0,0 +1,73 @@
+use async_nats::jetstream::{self};
+use esrc_ext::feature::Feature;
+
+pub mod translations;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    tracing::info!("Starting application server");
+
+    // * INFRA SETUP
+
+    // NATS connection setup
+    let nats_url = std::env::var("NATS_URL").expect("NATS_URL must be set");
+    let client = async_nats::connect(&nats_url).await?;
+    let context = jetstream::new(client.clone());
+
+    // Initialize the features and event store for your actual application
+    let event_store = esrc::nats::NatsStore::try_new(context.clone(), "users")
+        .await?
+        .update_durable_consumer_option(jetstream::consumer::pull::Config {
+            backoff: vec![std::time::Duration::from_secs(2)],
+            max_deliver: 2,
+            deliver_policy: jetstream::consumer::DeliverPolicy::New,
+
+            ..Default::default()
+        });
+
+    // External Store
+    let consumer = async_nats::jetstream::consumer::pull::Config {
+        deliver_policy: async_nats::jetstream::consumer::DeliverPolicy::All,
+        ..Default::default()
+    };
+
+    let external_stream = async_nats::jetstream::stream::Config {
+        name: "external".to_string(),
+        subjects: vec!["external.create_user".to_string()],
+        retention: async_nats::jetstream::stream::RetentionPolicy::WorkQueue,
+        allow_direct: true,
+        ..Default::default()
+    };
+
+    let external_store =
+        esrc_ext::translation::ExternalStore::try_new(context.clone(), &external_stream, &consumer)
+            .await?;
+
+    // * Dependencies
+    // Create CommandBus and attach handlers
+    let feature = Feature::new(&event_store);
+
+    // * Translations Project
+    // User translation setup
+    let user_project = translations::create_user::UserProject::new(event_store.clone()).await;
+    feature.start_translation(&external_store, user_project.clone(), "create_user");
+
+    // * Start Application
+    // Spawn a task to handle CTRL+C for graceful shutdown
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => {
+            println!("\nShutdown signal received...");
+        }
+    }
+
+    // Wait for all automations to exit gracefully
+    tracing::warn!("Waiting for graceful shutdown...");
+    event_store.wait_graceful_shutdown().await;
+
+    tracing::warn!("Application shut down successfully");
+    Ok(())
+}
---
+++ examples/translation_project/translations/create_user/project.rs
@@ -0,0 +1,52 @@
+use esrc::event::{Publish, ReplayOne};
+use esrc_ext::translation::TranslationProject;
+
+use super::*;
+
+#[derive(Debug, thiserror::Error)]
+pub enum UserErrors {
+    #[error(transparent)]
+    EventStore(#[from] esrc::Error),
+    #[error(transparent)]
+    Sqlx(#[from] sqlx::Error),
+}
+
+// Example project implementation
+#[derive(Clone)]
+pub struct UserProject<T>
+where
+    T: Clone + Publish + ReplayOne + Sync + 'static,
+{
+    /// Event store that you can publish event to an stream
+    _event_store: T,
+}
+
+impl<T> UserProject<T>
+where
+    T: Clone + Publish + ReplayOne + Sync + 'static,
+{
+    pub async fn new(event_store: T) -> Self {
+        Self {
+            _event_store: event_store,
+        }
+    }
+}
+
+impl<T> TranslationProject for UserProject<T>
+where
+    T: Clone + Publish + ReplayOne + Sync + 'static,
+{
+    type MessageGroup = ExternalEvents;
+    type Error = UserErrors;
+
+    async fn project(
+        &mut self,
+        event: Self::MessageGroup,
+        metadata: Option<serde_json::Value>,
+    ) -> Result<(), Self::Error> {
+        println!("Processing event: {:?}", event);
+        println!("With metadata: {:?}", metadata);
+
+        Ok(())
+    }
+}
